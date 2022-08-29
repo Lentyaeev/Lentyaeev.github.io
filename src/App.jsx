91c3db9b7f1b1929306238dd9504ab97952ac169
lentyaeev <daniilshaliuk@gmail.com>
2022-08-29 11:37:27 +0300

    Donee

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import { useEffect, useState } from 'react';
+import './App.scss';
+
+export const App = () => {
+  const [repos, setRepos] = useState([]);
+  const getRepos = () => {
+    return fetch('https://api.github.com/users/lentyaeev/repos?per_page=100')
+      .then((response) => response);
+  };
+
+  useEffect(() => {
+    getRepos().then(response => response.json())
+      .catch((er) => console.log(er))
+      .then(response => setRepos(response));
+  }, []);
+
+  const sortedRepos = repos.sort((a, b) => (
+    new Date(b.pushed_at) - new Date(a.pushed_at)
+  ));
+
+  return (
+    <div className="panel">
+      <ul>
+        {sortedRepos.length !== 0
+          ? (
+            <>
+              {sortedRepos.map(repo => (
+                <li>
+                  <a href={`https://lentyaeev.github.io/${repo.name}`}>{repo.name}</a>
+                </li>
+              ))}
+            </>
+          )
+          : (<h1>Loading.. </h1>)}
+      </ul>
+    </div>
+  );
+};
